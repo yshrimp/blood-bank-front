3d52161d0c7b609a476973ce9d51042b22a9b827
EC2 Default User <ec2-user@ip-10-0-240-12.ap-northeast-2.compute.internal>
2024-09-28 11:56:10 +0000

    v4

--- src/components/user/aws-exports.js
+++ src/components/user/aws-exports.js
@@ -1,7 +1,7 @@
 // src/aws-exports.js
 export const awsConfig = {
-    UserPoolId: "ap-northeast-2_zUkIj939I", // 사용자 풀 ID
-    ClientId: "11mhuk9qj96842febmjc4and5o", // 앱 클라이언트 ID
+    UserPoolId: "ap-northeast-2_8RPziZWWL", // 사용자 풀 ID
+    ClientId: "1lj4ctddmu9lr5ddhocli2ubol", // 앱 클라이언트 ID
     // 필요한 다른 설정을 추가합니다
   };
   
